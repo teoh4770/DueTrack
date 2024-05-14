7cfd664aea253d999f604d3961c9413ede84bd58
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-05-14 10:04:52 -0400

    add: handle components' import

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+// Icons
+import { BinIcon } from "./BinIcon.tsx";
+import { CrossIcon } from "./CrossIcon.tsx";
+import { InfoIcon } from "./InfoIcon.tsx";
+import { PlusIcon } from "./PlusIcon.tsx";
+import { ChevronDown } from "./ChevronDown.tsx";
+export { BinIcon, CrossIcon, PlusIcon, InfoIcon, ChevronDown };
---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+// UI
+import { Button } from "./ui/Button/Button.tsx";
+import { Checkbox } from "./ui/Checkbox/Checkbox.tsx";
+import { ColorInput } from "./ui/ColorInput/ColorInput.tsx";
+import { DateInput } from "./ui/DateInput/DateInput.tsx";
+import { TextInput } from "./ui/TextInput/TextInput.tsx";
+export { Button, Checkbox, ColorInput, DateInput, TextInput };
+
+// Components
+import { CurrentDate } from "./CurrentDate.tsx";
+import { Deadline } from "./Deadline/Deadline.section.tsx";
+import { DeadlineItem } from "./Deadline/DeadlineItem.tsx";
+import { DeadlineList } from "./Deadline/DeadlineList.tsx";
+import { DeadlineOptions } from "./Deadline/DeadlineOptions.tsx";
+export { CurrentDate, Deadline, DeadlineItem, DeadlineList, DeadlineOptions };
